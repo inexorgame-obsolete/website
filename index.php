4b66505307580dc67545e0fb2707cb20c0d46f61
Fohlen <lennard@berger.work>
2015-08-13 17:27:18 +0200

    Updated all stuff to work via composer directly

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
 
-// Kickstart the framework
-$f3=require('lib/base.php');
-
-// Bootstrap composer
-include_once('vendor/autoload.php');
+// Composer autoloader for required packages and dependencies
+require_once('lib/autoload.php');
+$f3 = \Base::instance();
 
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
