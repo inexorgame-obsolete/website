c3d974adcf24f85e9aa9f902152157f5a3613857
Fohlen <lennard@berger.work>
2015-08-15 13:23:24 +0200

    Cleaned up old rules. Added a link to the nav.

--- index.php
+++ index.php
@@ -11,9 +11,6 @@ if ((float)PCRE_VERSION<7.9)
 // Load configuration
 $f3->config('config.ini');
 
-/*require_once("app/readPost.php");
-require_once("app/posts.php");*/
-
 $f3->route('GET /', 'Controllers\Blog->index');
 $f3->route('GET /blog/@year', 'Controllers\Blog->year');
 $f3->route('GET /blog/@year/@entry', 'Controllers\Blog->year');
@@ -22,17 +19,9 @@ $f3->route('GET /@controller/@action', 'Controllers\@controller->@action');
 $f3->route('GET /@controller/@action/@param', 'Controllers\@controller->@action');
 
 /*
-
 $f3->route('GET / [ajax]', function($f3) {
 	
 });
-	
-$f3->route('GET|POST /preview', function($f3) {
-	$content = (null !== $f3->get('POST.content')) ? $f3->get('POST.content') : 'No content available.';
-	$f3->set('content', $content);
-	echo View::instance()->render('preview.htm');
-});
-	
 */
 
 $f3->run();
