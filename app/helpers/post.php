71f57fc645c45f9df0660ef81c6ae2509ddae8fd
Fohlen <lennard@berger.work>
2015-08-20 14:29:24 +0200

    Added a way to set default values..

--- app/helpers/post.php
+++ app/helpers/post.php
@@ -25,6 +25,12 @@ class Post extends \Prefab {
 				$this->_meta[$key] = $value;
 			}
 			
+			// Set default for empty values
+			foreach ($this->_meta as $meta) {
+				if (empty($meta)) 
+					$this->_meta[$meta] = $this->_setDefault($meta);
+			}
+			
 			$this->content = stream_get_contents($handle);
 			fclose($handle);
 
@@ -32,6 +38,16 @@ class Post extends \Prefab {
 			$this->link = str_replace('.md', '', $this->link);
 		} catch (\Exception $e) {
 			// Need's error logging
+		}
+	}
+	
+	private function _setDefault($key) {
+		switch($key) {
+			case 'layout': return 'post';
+			case 'title': return 'A blog entry';
+			case 'date': return \DateTime('now')->format('Y-m-d H:i:s O');
+			case 'author': return 'Inexor team';
+			case 'summary': return 'No summary available';
 		}
 	}
 	
