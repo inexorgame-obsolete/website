898281219dcd749096e5df54d90af0bdca6a94af
Fohlen <lennard@berger.work>
2015-10-29 13:51:53 +0100

    Added a helper class to retrieve the latest url..

--- app/controllers/landing.php
+++ app/controllers/landing.php
@@ -3,7 +3,16 @@ namespace Controllers;
 
 class Landing {
 	public function index() {
+				
+		// Retrieve the current release URL using Github's API
+		if (! \Cache::instance()->exists('release')) {
+			$release = \Helpers\Release::instance();
+			\Cache::instance()->set('release', $release, 86400); //TTL = 1 day
+		} else {
+			$release = \Cache::instance()->get('release');
+		}
 		
+		\Base::instance()->set('release_url', $release->html_url);
 		echo \View::instance()->render('landing.htm');
 	}
 }
---
+++ app/helpers/release.php
@@ -0,0 +1,30 @@
+<?php
+namespace Helpers;
+use \Curl\Curl;
+
+class Release extends \Prefab {
+	private $_meta;
+	
+	public function __construct() {
+		try {
+			$curl = new Curl();
+			$releases = $curl->get('https://api.github.com/repos/inexor-game/code/releases');
+			/* 
+			 * This is a nasty little "hack". $releases is an stdObject filled with stacks of
+			 * stdObjects. Using {0} as an index and using quick convert (array) will return
+			 * the first result from the Github API as an Array
+			 */
+			$this->_meta = (array)$releases{0};
+				
+			if ($curl->error) throw(new \Exception($curl->errorMessage));
+		} catch (\Exception $e) {
+			// TODO: Add error handling
+		}
+	}
+	
+	public function __get($key) {
+		if (key_exists($key, $this->_meta)) {
+			return $this->_meta[$key];
+		}
+	}
+}
