873457cbf370b9971ec4e6c0d355e55f775fa7e6
Fohlen <lennard@berger.work>
2015-11-05 22:45:34 +0100

    Added some todo notes ... check before release

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
 
+/**
+ * TODO: Notes
+ * Add various error routines to the file handler functions (if not, look up the helpers)
+ * Add a caching for the feed module..
+ * Add a custom 404 page and add a route for it
+ * Implement a download page
+ * 	- use an FTP library to connect to our host
+ * 	- filter all master builds
+ * 	- build a list with font-awesome distribution icons
+ */
 // Load configuration
 $f3->config('config.ini');
 
