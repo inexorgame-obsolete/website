faf3ddb8bb184b8ef247cec5edd35f0ec331f3c7
Fohlen <lennard@berger.work>
2015-11-06 14:09:41 +0100

    Added caching to the feed controller..

--- app/controllers/feed.php
+++ app/controllers/feed.php
@@ -4,31 +4,44 @@ use PicoFeed\Syndication;
 use Helpers;
 
 class Feed {
+	private $_uri;
 	private $_posts = array();
 
 	public function __construct()
 	{
+		// Initialise the base URL. This should be done every single time since http/https could change..
+		$f3 = \Base::instance();
+		$this->_uri = $f3->get('SCHEME').'://'.$f3->get('HOST').':'.$f3->get('PORT').$f3->get('BASE').'/';
+		
 		// TODO: This could be cleaned up and cache could be added (add base_uri)
 		$entries = \Helpers\Posts::instance()->getLatest(15);
+		if (!\Cache::instance()->exists('feeds')) {
+			$feeds = array();	
+		
+			foreach($entries as $entry) {
+				$meta = new \Helpers\Post($entry);
 			
-		foreach($entries as $entry) {
-			$meta = new \Helpers\Post($entry);
+				// Get the markdown stuff
+				$content = \Markdown::instance()->convert($meta->content);
 			
-			// Get the markdown stuff
-			$content = \Markdown::instance()->convert($meta->content);
-			
-			$item = array(
-				'title' => $meta->title,
-				'updated' => strtotime($meta->date),
-				'url' => $uri . $meta->link,
-				'content' => $content,
-				'author' => array(
-					'name' => $meta->author
-				)
-			);
+				$item = array(
+					'title' => $meta->title,
+					'updated' => strtotime($meta->date),
+					'url' => $uri . $meta->link,
+					'content' => $content,
+					'author' => array(
+						'name' => $meta->author
+					)
+				);
 				
-			array_push($this->_posts, $item);
-		}	
+				array_push($feeds, $item);
+			}
+			
+			\Cache::instance()->set('feeds', $feeds, 600); //TTL = 10 minutes
+			$this->_posts = $feeds;
+		} else {
+			$this->_posts = \Cache::instance()->get('feeds');
+		}
 	}
 	
 	public function index()
@@ -43,11 +56,8 @@ class Feed {
 		$writer->title = "Inexor";
 		$writer->description = "Stays sauer. Becomes better";
 		
-		// The currently used uri
-		$f3 = \Base::instance();
-		$uri = $f3->get('SCHEME').'://'.$f3->get('HOST').':'.$f3->get('PORT').$f3->get('BASE').'/';
-		$writer->site_url = $uri;
-		$writer->feed_url = $uri . 'feed/rss';
+		$writer->site_url = $this->_uri;
+		$writer->feed_url = $this->_uri . 'feed/rss';
 		
 		$posts = \Helpers\Posts::instance()->getLatest(15);
 		$writer->items = $this->_posts;
@@ -62,10 +72,8 @@ class Feed {
 		$writer->description = "Stays sauer. Becomes better";
 		
 		// The currently used uri
-		$f3 = \Base::instance();
-		$uri = $f3->get('SCHEME').'://'.$f3->get('HOST').':'.$f3->get('PORT').$f3->get('BASE').'/';
-		$writer->site_url = $uri;
-		$writer->feed_url = $uri . 'feed/atom';
+		$writer->site_url = $this->_uri;
+		$writer->feed_url = $this->_uri . 'feed/atom';
 		
 		$posts = \Helpers\Posts::instance()->getLatest(15);
 		$writer->items = $this->_posts;
