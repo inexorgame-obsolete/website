376d4ffa88658a9bbcf1606ff73f25cd0cf3ab5c
Fohlen <lennard@berger.work>
2015-11-29 13:42:17 +0100

    Added template for the team view

--- app/controllers/team.php
+++ app/controllers/team.php
@@ -5,6 +5,7 @@ use \Curl\Curl;
 class Team {
 	public function index()
 	{
+		apcu_clear_cache('user');
 		if (!\Cache::instance()->exists('members')) {
 			try {
 				$curl = new Curl;
@@ -18,15 +19,20 @@ class Team {
 				\Base::instance()->error(500);
 			}
 
+			// Merge members with their aliases
+			$aliases = $this->getAliases();
+			foreach ($members as $member) {
+				$member->alias = $aliases[$member->id]['names']['inexor'];
+			}
+			
 			\Cache::instance()->set('members', $members, 3600); //TTL = 1h
 		} else {
 			$members = \Cache::instance()->get('members');
 		}
 		
 		\Base::instance()->set('members', $members);
-		\Base::instance()->set('aliases', $this->getAliases());
 		\Base::instance()->set('content', 'members.htm');
-		echo \View::instance()->render('layout.htm');
+		echo \Template::instance()->render('layout.htm');
 	}
 	
 	private function getAliases() {
